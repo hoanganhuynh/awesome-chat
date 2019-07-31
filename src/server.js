af765c603cd5851c1949e847ed5a6ca30bf5bcff
Hoang An <hoangan@Hoangs-Mac-mini.local>
2019-07-31 12:53:08 +0700

    [1.0.0] Add static file

--- src/server.js
+++ src/server.js
@@ -1,25 +1,22 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine";
 
+// Init app
 let app = express();
 
 // Connect to MongoDB
 ConnectDB();
 
-app.get("/test-database", async (req, res) => {
-  try {
-    let item = {
-      userId: "1333113123232",
-      contactId: "1313132232344546576"
-    };
-    let contact = await ContactModel.createNew(item);
-    res.send(contact);
-  } catch (err) {
-    console.log(err);
-  }
+// Config View engine
+configViewEngine(app);
 
-  
+app.get("/", (req, res) => {
+  return res.render("main/master");
+});
+
+app.get("/login-register", (req, res) => {
+  return res.render("auth/loginRegister");
 });
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
