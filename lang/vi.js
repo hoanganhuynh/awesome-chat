881963519cc35239f3ccedbab92eb171db7c0d1b
Hoang An <hoangan@Hoangs-Mac-mini.local>
2019-08-01 12:51:35 +0700

    [1.0.0] Logout Function.

--- lang/vi.js
+++ lang/vi.js
@@ -21,7 +21,8 @@ export const transSuccess = {
   account_actived: "Kích hoạt tài khoản thành công, bạn đã có thể đăng nhập.",
   login_success: (username)=> {
     return `Xin chào ${username}, chúc bạn một ngày tốt lành.`
-  }
+  },
+  logout_success: "Đăng xuất tài khoản thành công, hẹn gặp lại bạn :)"
 };
 
 export const transMail = {
